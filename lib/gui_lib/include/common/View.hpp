37c6f856ad7b29a97fee6bde2a13fac45ae1ea5c
K3fas <peter.branecky2@gmail.com>
2023-04-29 12:57:06 +0200

    Added rule of 3 to margin

--- lib/gui_lib/include/common/View.hpp
+++ lib/gui_lib/include/common/View.hpp
@@ -20,6 +20,8 @@ namespace rpgui::common
         uint8_t l, t, r, b;
 
         Margin() : l(0), t(0), r(0), b(0) {}
+        Margin( const Margin&) = default;
+        ~Margin() = default;
         Margin(uint8_t l, uint8_t t, uint8_t r, uint8_t b)
             : l(l), t(t), r(r), b(b) {}
     };
